f187c6ac4ad8393f218a1b085df4a92661ba9c74
Bobby Reed <Bobby.Reed@microsoft.com>
2017-07-07 11:23:07 -0400

    Fixed GPO Audit Parser

--- src/Parsers/GPO/Audit.ps1
+++ src/Parsers/GPO/Audit.ps1
@@ -10,45 +10,51 @@ Function Write-GPOAuditCSVData
 
     $retHash = @{}
     $retHash.Name = $Entry.SubCategory
+    $Name = $Entry.SubCategory
 
-    switch -regex ($Entry.InclusionSetting)
+    switch -regex ($Entry."Inclusion Setting")
     {
         "Success and Failure"
         {
             $retHash.Ensure = "Present"
             $retHash.AuditFlag = "Success"
+            $duplicate = $retHash.Clone()
+            $duplicate.AuditFlag = "Failure"
             Write-DSCString -Resource -Name "$Name (Success) - Inclusion" -Type AuditPolicySubcategory -Parameters $retHash
-            $retHash.AuditFlag = "Failure"
-            Write-DSCString -Resource -Name "$Name (Failure) - Inclusion" -Type AuditPolicySubcategory -Parameters $retHash
+            Write-DSCString -Resource -Name "$Name (Failure) - Inclusion" -Type AuditPolicySubcategory -Parameters $duplicate
         }
 
         "No Auditing"
         {
             $retHash.Ensure = "Absent"
             $retHash.AuditFlag = "Success"
+            $duplicate = $retHash.Clone()
+            $duplicate.AuditFlag = "Failure"
             Write-DSCString -Resource -Name "$Name (Success) - Inclusion" -Type AuditPolicySubcategory -Parameters $retHash
             $retHash.AuditFlag = "Failure"
-            Write-DSCString -Resource -Name "$Name (Failure) - Inclusion" -Type AuditPolicySubcategory -Parameters $retHash
+            Write-DSCString -Resource -Name "$Name (Failure) - Inclusion" -Type AuditPolicySubcategory -Parameters $duplicate
         }
 
         "^(Success|Failure)$"
         {
             $retHash.Ensure = "Present"
-            $retHash.AuditFlag = $Entry.InclusionSetting
+            $retHash.AuditFlag = $Entry."Inclusion Setting"
             Write-DSCString -Resource -Name "$Name - Inclusion" -Type AuditPolicySubcategory -Parameters $retHash
         }
     }
     
-    $retHash.Ensure = "Absent"
-    switch -regex ($Entry.ExclusionSetting)
+    $exclusionHash = $retHash.Clone()
+    switch -regex ($Entry."Exclusion Setting")
     {
         "Success and Failure"
         {
-            $retHash.Ensure = "Absent"
-            $retHash.AuditFlag = "Success"
-            Write-DSCString -Resource -Name "$Name (Success) - Exclusion" -Type AuditPolicySubcategory -Parameters $retHash
+            $exclusionHash.Ensure = "Absent"
+            $exclusionHash.AuditFlag = "Success"
+            $exclusionHashduplicate = $exclusionHash.Clone()
+            $exclusionHashduplicate.AuditFlag = "Failure"
+            Write-DSCString -Resource -Name "$Name (Success) - Exclusion" -Type AuditPolicySubcategory -Parameters $exclusionHash
             $retHash.AuditFlag = "Failure"
-            Write-DSCString -Resource -Name "$Name (Failure) - Exclusion" -Type AuditPolicySubcategory -Parameters $retHash
+            Write-DSCString -Resource -Name "$Name (Failure) - Exclusion" -Type AuditPolicySubcategory -Parameters $exclusionHashduplicate
         }
 
         "No Auditing"
@@ -58,9 +64,9 @@ Function Write-GPOAuditCSVData
 
         "^(Success|Failure)$"
         {
-            $retHash.Ensure = "Absent"
-            $retHash.AuditFlag = $Entry.ExclusionSetting
-            Write-DSCString -Resource -Name "$Name - Exclusion" -Type AuditPolicySubcategory -Parameters $retHash
+            $exclusionHash.Ensure = "Absent"
+            $exclusionHash.AuditFlag = $Entry."Exclusion Setting"
+            Write-DSCString -Resource -Name "$Name - Exclusion" -Type AuditPolicySubcategory -Parameters $exclusionHash
         }
     }
 }
