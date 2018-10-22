6eac50dbe4de6968e1689e79b69922b3f1e86f11
Bobby Reed <bobby.reed@microsoft.com>
2018-10-22 14:06:06 -0400

    Fixing parameter error

--- src/BaselineManagement.psm1
+++ src/BaselineManagement.psm1
@@ -144,7 +144,7 @@ function ConvertFrom-GPO
     (
         # This is the Path of the GPO Backup Directory.
         [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true, ParameterSetName = "Path")]
-        [ValidateScript( {Test-Path $_ -Container})]
+        [ValidateScript( {Test-Path $_ -PathType Container})]
         [System.IO.DirectoryInfo[]]$Path,
         
         # This is the GPO Object returned from Backup-GPO.
