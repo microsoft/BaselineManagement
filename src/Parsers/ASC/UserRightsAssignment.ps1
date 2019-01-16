8feac3d6478de54884edbe26379cdb7efce5fdfc
bdanse <bartdanse@hotmail.com>
2019-01-16 15:36:52 +0100

    Always force UserRights Accommodate additional account values

--- src/Parsers/ASC/UserRightsAssignment.ps1
+++ src/Parsers/ASC/UserRightsAssignment.ps1
@@ -23,7 +23,8 @@ Function Write-ASCPrivilegeJSONData
     switch (($PrivilegeData.ExpectedValue -split ",\s*"))
     {
         "No One" { $Accounts = ""; break }
-        "SERVICE" { $Accounts += "NT AUTHORITY\SERVICE" } 
+        "0" { $Accounts = ""; break }
+        "SERVICE" { $Accounts += "NT AUTHORITY\SERVICE" }
         "NEW_VALUE" { }
         "LOCAL SERVICE" { $Accounts += "NT AUTHORITY\LOCAL SERVICE" }
         "AUTHENTICATED USERS" { $Accounts += "NT AUTHORITY\AUTHENTICATED USERS" }
@@ -31,19 +32,23 @@ Function Write-ASCPrivilegeJSONData
         "NETWORK SERVICE" { $Accounts += "NT AUTHORITY\NETWORK SERVICE" }
         "NT AUTHORITY\Local account and member of Administrators group" { $Accounts += "[Local Account|Administrator]" }
         "NT AUTHORITY\Local account" { $Accounts += "[Local Account]"}
+        "Local account" { $Accounts += "[Local Account]"}
         "Guests" { $Accounts += "BUILTIN\Guests"}
         "Backup Operators" { $Accounts += "BUILTIN\Backup Operators"}
+        "Server Operators" { $Accounts += "BUILTIN\Server Operators"}
+        "NT SERVICE\WdiServiceHost" { $Accounts += "NT SERVICE\WdiServiceHost" }
+        "NT VIRTUAL MACHINE\\Virtual Machines" { $Accounts += "NT VIRTUAL MACHINE\Virtual Machines" }
+        "Remote Desktop Users" { $Accounts += "BUILTIN\Remote Desktop Users" }
+        "Print Operators" { $Accounts += "BUILTIN\Print Operators" }
+        "IIS APPPOOL\DefaultAppPool" { $Accounts += "IIS APPPOOL\DefaultAppPool" }
         Default { Write-Warning "Found a new Account Value for JSONPrivilege: $_" }
     }
-                                
+
     $policyHash = @{}
-    if ([string]::IsNullOrEmpty($Accounts))
-    {
-        $policyHash.Force = $true
-    }    
-    
+    $policyHash.Force = $true
+
     $policyHash.Policy = $Privilege
-    $policyHash.Identity = $Accounts                    
-                    
+    $policyHash.Identity = $Accounts
+
     return Write-DSCString -Resource -Name "$($PrivilegeData.CCEID): $($PrivilegeData.ruleName)" -Type UserRightsAssignment -Parameters $policyHash -CommentOUT:($PrivilegeData.State -ne 'Enabled') -DoubleQuoted
 }
