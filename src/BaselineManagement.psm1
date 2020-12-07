0906a87f2d2080ee68becf424c446db6f74fb8b6
Michael Greene <michael.greene@microsoft.com>
2020-12-07 11:33:05 -0600

    name

--- src/BaselineManagement.psm1
+++ src/BaselineManagement.psm1
@@ -233,9 +233,9 @@ function ConvertFrom-GPO
 
                         "Registry Values"
                         {
-                            $securityOptionName = $securityOptionData.GetEnumerator() | Where-Object {$_.Value.Value -eq $subkey}
+                            $securityOption = $securityOptionData.GetEnumerator() | Where-Object {$_.Value.Value -eq $subkey}
                             if ($securityOptionName) {
-                                    $ConfigString += Write-GPORegistryINFData -Name $securityOptionName -ValueData $ini[$key][$subKey]
+                                    $ConfigString += Write-GPORegistryINFData -Name $securityOption.Name -ValueData $ini[$key][$subKey]
                             }
                             else {
                                 throw "The GptTmpl.inf file contains an entry '$subkey' in the Registry section that is an unknown value."
